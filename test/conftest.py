85f759ed93e14656d063158dc10ea277218995d0
Michael Xia <michaelxswe@gmail.com>
2024-01-02 18:50:14 -0500

    update

--- test/conftest.py
+++ test/conftest.py
@@ -20,6 +20,6 @@ async def app():
 
 @pytest.fixture(scope="session")
 async def client(app: FastAPI):
-    async with lifespan(app): #test start up event as well
+    async with lifespan(app): #test startup event as well
         async with AsyncClient(app=app, base_url="http://localhost") as client:
             yield client
